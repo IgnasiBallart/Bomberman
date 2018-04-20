e888d11206ec21c383fb7b2b965b1c46a2d852d5
IgnasiBallart <ignasi.ballart@gmail.com>
2018-04-20 09:46:57 +0200

    First commit

---
+++ Assets/BombSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour {
+
+    public Tilemap tilemap;
+
+    public GameObject bombPrefab;
+
+    private Character character;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        character = FindObjectOfType<Character>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 worldPose = character.getCharacterPos();
+            Vector3Int cell = tilemap.WorldToCell(worldPose);
+            Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        }
+		
+	}
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Character : MonoBehaviour {
+
+    public Tilemap tilemap;
+
+    public GameObject characterPrefab;
+
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidbody;
+
+    public Character character;
+    
+    // Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") >  -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+        }
+    }
+
+    public Vector3 getCharacterPos()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y, 0);
+        return pos;
+    }
+}
